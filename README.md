b7c7e69ca0fd9466696ea51b760fb0332161dba3
Rafael Silva <rasilva.1986@email.com>
2021-11-03 15:39:52 -0700

    Create the structure of the project

---
+++ README.md
@@ -0,0 +1,29 @@
+# Project - Edugrade - Linux 2 - Team 1
+## Authors:
+| Name            | Email address                | GitHub username |
+| --------------- |:----------------------------:| ---------------:|
+| Rafael Silva    |rasilva.1986@gmail.com        |rasi10           |
+| Adil Riaz       |adilriaz001@hotmail.com       |adilriaz001      |
+| Ahmed Aaden     |ahmed.aaden@edugrade.se       |ahaad74          |
+| Joakim Larsson  |joakim.larsson@edu.edugrade.se|youreakim        |
+| Ghulam Mustafa  |       |     |
+| Joshua Lutakome |       |     |
+
+
+
+## Required packages to be installed:
+```
+openssl
+ssmtp
+mailutils
+curl
+```
+
+
+## How to run the project:
+```
+chmod 777 certificate-monitor.sh
+sh certificate-monitor.sh
+```
+
+v1 - _November 3rd, 2021_
---
+++ certificate-monitor.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#***************************************************************************#   
+#                       certificate-monitor.sh                              #
+#                                                                           #       
+#                      Edugrade - Linux 2 - Team 1                          #
+#                            Adil Riaz                                      #
+#                           Ahmed Aaden                                     #
+#                           Rafael Silva                                    #
+#                          Joakim Larsson                                   #
+#                          Ghulam Mustafa                                   #
+#                          Joshua Lutakome                                  #
+#                                                                           #
+#                       November 3rd, 2021                                  #
+#***************************************************************************#
+
+#----------------------------------------------------------------------------#
+#      read_input_file()                                                     #
+#                                                                            #
+#      TO DO -> ADD COMMENTS LATER                                           #
+#                                                                            #
+#----------------------------------------------------------------------------#
+function read_input_file(){
+    for i in $(cat site-list.txt);
+    do
+        url=$i
+        echo $url
+    done    
+
+    echo 'This method should return an array with the values stored in the file'
+}
+
+#----------------------------------------------------------------------------#
+#      check_expiring_date_for_certificates()                                #
+#                                                                            #
+#      TO DO -> ADD COMMENTS LATER                                           #
+#                                                                            #
+#----------------------------------------------------------------------------#
+function check_expiring_date_for_certificates() {
+    echo 'this method takes in an array as parameter and returns a string which is the body of the email'
+    echo 'In here we should use openssl to build this information and use a function to calculate how many days'
+}
+
+#----------------------------------------------------------------------------#
+#      calculate_days_left()                                                 #
+#                                                                            #
+#      TO DO -> ADD COMMENTS LATER                                           #
+#                                                                            #
+#----------------------------------------------------------------------------#
+function calculate_days_left(){
+    echo 'this method should calculate how many days are left for one certificate to expire'
+}
+
+#----------------------------------------------------------------------------#
+#      send_email_to_group_target()                                          #
+#                                                                            #
+#      TO DO -> ADD COMMENTS LATER                                           #
+#                                                                            #
+#----------------------------------------------------------------------------#
+function send_email_to_group_target() {
+    echo 'This method should use the smtp server to send an email to the target group'
+    echo 'The message that will be sent is received from the method check_expiring_date_for_certificates'
+}
+
+
+
+
+#----------------------------------------------------------------------------#
+#      MAIN - In here we invoke the defined methods in a logical order       #
+#----------------------------------------------------------------------------#
+read_input_file
